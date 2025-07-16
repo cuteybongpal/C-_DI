0c3fc5102c161a708475a4325ff397433a09bf33
hyowonKim <khw33352182@gmail.com>
2025-07-17 01:37:07 +0900

    feat(Pooler, ConstructorParam) :add Pooler.GetInstance(ConstructorParam)
    오브젝트 풀링을 이용해 객체를 리턴받는 함수.
    T타입의 생성자의 인자를 컴파일단계에서 확정할 수 없기 때문에, ConstructorParam라는 구조체를 만든 후, 이걸 상속받는 데이터 구조체를 만들어 사용할 수 있게 할 거임

--- DIContainer/Pooler.h
+++ DIContainer/Pooler.h
@@ -1,14 +1,29 @@
 #pragma once
 #include <queue>
+#include "ConstructorParam.h"
 
 template <typename T>
 class Pooler
 {
 public:
-	T* GetInstance();
-
-	Pooler();
-priavte:
-	std::queue<T> poolingQueue;
+	//°´Ã¼¸¦ °¡Á®¿È
+	T* GetInstance(ConstructorParam& param) 
+	{
+		if (poolingQueue.size() <= 0)
+			return new T(param);
+		
+		T* instance = poolingQueue.pop();
+		return instance;
+	}
+	//°´Ã¼ ¹ÝÈ¯
+	void ReturnInstance(T* instance)
+	{
+		poolingQueue.push(instance);
+	}
+	Pooler() {
+		poolingQueue = std::queue<T*>();
+	}
+private:
+	std::queue<T*> poolingQueue;
 };
 
