a302441c4c07702d789d560a5ec0cabd2df35c99
hyowonKim <khw33352182@gmail.com>
2025-07-17 01:35:26 +0900

    refactor(Dependency_ptr): move function implementations from cpp to h
    템플릿 함수는 T타입에 의존적일 시 헤더 파일에서 구현도 동시에 해줘야 하기 때문에 바꾼 후, 완성

--- DIContainer/Dependency_ptr.h
+++ DIContainer/Dependency_ptr.h
@@ -1,18 +1,41 @@
 #pragma once
 
-class NullObject;
-
 template<typename T>
 class Dependency_ptr
 {
 public:
-	T* operator->() const;
-	Dependency_ptr<T>* operator*() const;
-	void operator=(std::nullptr_t n);
+	T* operator->() const
+	{
+		return Tptr;
+	}
+	Dependency_ptr<T>* operator*() const
+	{
+		SetReferenceCount(GetReferenceCount() + 1);
+		return this;
+	}
+	void operator=(std::nullptr_t n)
+	{
+		SetReferenceCount(GetReferenceCount() - 1);
+	}
+	Dependency_ptr<T>(T* ptr) 
+	{
+		Tptr = ptr;
+		referenceCount = 1;
+	}
 private:
 	T* Tptr;
 	int referenceCount;
-	void SetReferenceCount(int value);
-	int GetReferenceCount();
+	void SetReferenceCount(int value) {
+		referenceCount = value;
+		if (referenceCount <= 0)
+		{
+			;
+			//todo : DIContainer¿¡ °´Ã¼ ¹ÝÈ¯
+		}
+	}
+	int GetReferenceCount() 
+	{
+		return referenceCount;
+	}
 };
 
